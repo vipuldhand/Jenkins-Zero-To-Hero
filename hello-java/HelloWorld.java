a4b9f9467f98529e655b6c85070fd800f632d54e
vipuldhand <vipuldhand@gmail.com>
2026-03-12 22:15:49 +0530

    Updated HelloWorld message

--- hello-java/HelloWorld.java
+++ hello-java/HelloWorld.java
@@ -1,15 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String[] colors = {"\u001B[31m", "\u001B[32m", "\u001B[33m", "\u001B[34m", "\u001B[35m", "\u001B[36m"};
-        String reset = "\u001B[0m";
-        String message = "Hello, World!";
-        
-        System.out.println("\n" + "╔" + "═".repeat(message.length() + 4) + "╗");
-        System.out.print("║  ");
-        for (int i = 0; i < message.length(); i++) {
-            System.out.print(colors[i % colors.length] + message.charAt(i) + reset);
-        }
-        System.out.println("  ║");
-        System.out.println("╚" + "═".repeat(message.length() + 4) + "╝\n");
+        System.out.println("hello world vipul here, learning jenkins");
     }
 }
